e97832750ff71dac57f61466217838462e805095
GusttavoBuenno <gustavobueno05@outlook.com.br>
2025-02-05 11:16:31 -0300

    feature:htmlTesting

--- scraper/views.py
+++ scraper/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render
-from .models import Produto  # Certifique-se de que o modelo Produto está correto
+from .models import Produto
 
 def lista_produtos(request):
-    produtos = Produto.objects.all()  # Busca todos os produtos no banco
-    return render(request, 'lista_produtos.html', {'produtos': produtos})
+    produtos = Produto.objects.all()
+    return render(request, 'scraper/lista_produtos.html', {'produtos': produtos})
